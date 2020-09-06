a1106158803563a4c8f590e262c096a89c7642e4
daiki-murakami <muranokami1@gmail.com>
2020-09-06 23:20:08 +0900

    DB接続

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+  "fmt"
+
+  "github.com/gin-gonic/gin"
+  "github.com/jinzhu/gorm"
+  _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+  _, err := sqlConnect()
+  if err != nil {
+      panic(err.Error())
+  } else {
+      fmt.Println("DB接続成功")
+  }
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{})
-    })
+  router := gin.Default()
+  router.LoadHTMLGlob("templates/*.html")
 
-    router.Run()
+  router.GET("/", func(ctx *gin.Context){
+      ctx.HTML(200, "index.html", gin.H{})
+  })
+
+  router.Run()
+}
+
+func sqlConnect() (database *gorm.DB, err error) {
+  DBMS := "mysql"
+  USER := "go_test"
+  PASS := "password"
+  PROTOCOL := "tcp(db:3306)"
+  DBNAME := "go_database"
+
+  CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+  return gorm.Open(DBMS, CONNECT)
 }
